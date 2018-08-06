b6d269059e62e229bfa9467acafb7b7b4be0895c
Keith <keithtmccartney@hotmail.com>
2018-08-06 22:04:33 +0100

    Applying changes to the 'ui' service, alongside changes to the 'app-routing' module to tell Angular what are the different routes (URLs) and their associated components (pages).

--- Minimus/src/app/ui.service.ts
+++ Minimus/src/app/ui.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class UiService {
+  darkModeState: BehaviorSubject<boolean>;
 
-  constructor() { }
+  constructor() {
+    // TODO: if the user is signed in get the default value from Firebase
+    this.darkModeState = new BehaviorSubject<boolean>(false);
+  }
 }
